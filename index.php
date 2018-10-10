89228d2e763028a488e5d277747c61af180455a5
rahuldesai02 <31479983+rahuldesai02@users.noreply.github.com>
2018-10-10 12:23:05 +0530

    Added Owner,Some more attributes for bus table

--- index.php
+++ index.php
@@ -25,24 +25,36 @@
 				document.getElementById("insertConductor").style.display = "none";
 				document.getElementById("insertBusStop").style.display = "none";
 				document.getElementById("insertBus").style.display = "block";
+				document.getElementById("insertOwner").style.display = "none";
+				
 			}
 			function insertDriver(){
 				document.getElementById("insertConductor").style.display = "none";
 				document.getElementById("insertBusStop").style.display = "none";
 				document.getElementById("insertBus").style.display = "none";
+				document.getElementById("insertOwner").style.display = "none";
 				document.getElementById("insertDriver").style.display = "block";
 			}
 			function insertConductor(){
 				document.getElementById("insertBusStop").style.display = "none";
 				document.getElementById("insertBus").style.display = "none";
 				document.getElementById("insertDriver").style.display = "none";
+				document.getElementById("insertOwner").style.display = "none";
 				document.getElementById("insertConductor").style.display = "block";
 			}
 			function insertBusStop(){
 				document.getElementById("insertBus").style.display = "none";
 				document.getElementById("insertDriver").style.display = "none";
 				document.getElementById("insertConductor").style.display = "none";
+				document.getElementById("insertOwner").style.display = "none";
 				document.getElementById("insertBusStop").style.display = "block";
+			}
+			function insertOwner(){
+				document.getElementById("insertBus").style.display = "none";
+				document.getElementById("insertDriver").style.display = "none";
+				document.getElementById("insertConductor").style.display = "none";
+				document.getElementById("insertBusStop").style.display = "none";
+				document.getElementById("insertOwner").style.display = "block";
 			}
 		</script>
 	</head>
@@ -52,6 +64,7 @@
 		<button type = "button" onclick = "insertDriver()">Click here to insert driver information</button>
 		<button type = "button" onclick = "insertConductor()">Click here to insert conductor information</button>
 		<button type = "button" onclick = "insertBusStop()">Click here to insert bus stop information</button>
+		<button type = "button" onclick = "insertOwner()">Click here to insert Owner information</button>
 		<div id = "insertBus">
 		<h1>Enter Bus Details</h1>
 		<form action="InsertBus.php" method="post">
@@ -60,6 +73,9 @@
 			Registration No:<input type="text" name="regno"/><br><br>
 			Bus Manufacturer:<input type="text" name="busmanufacturer"/><br><br>
 			Bus Color:<input type="text" name="buscolour"/><br><br>
+			Driver ID:<input type="text" name="driverid"/><br><br>
+			Conductor ID:<input type="text" name="conductorid"/><br><br>
+			Owner ID:<input type="text" name="ownerid"><br><br>
 			<input type="Submit"/>
 		</form>
 		</div>
@@ -90,6 +106,15 @@
 			Bus_Stop_ID:<input type="text" name="busstopid"/><br><br>
 			Place:<input type="text" name="place"/><br><br>
 			Coordinates:<input type="text" name="coordinates"/><br><br>
+			<input type="Submit"/>
+		</form>
+		</div>
+		<div id = "insertOwner">
+		<h1>Enter Owner Details</h1>
+		<form action="InsertOwner.php" method="post">
+			Owner_ID:<input type="text" name="ownerid"/><br><br>
+			Name:<input type="text" name="ownername"/><br><br>
+			Contact No:<input type="text" name="contactno"/><br><br>
 			<input type="Submit"/>
 		</form>
 		</div>
