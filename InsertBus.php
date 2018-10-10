463a2399d9315332bbf32b4696cd97a075f97954
rahuldesai02 <31479983+rahuldesai02@users.noreply.github.com>
2018-10-10 12:25:10 +0530

    Added Bus Attributes

--- InsertBus.php
+++ InsertBus.php
@@ -5,7 +5,7 @@
 	{
 		die("ERROR:COULD NOT CONNECT".mysqli_connect_error());
 	}
-	$sql="INSERT INTO bus(Reg_No,Bus_Name,Bus_Id,Bus_Colour,Bus_Manufacturer) values ('$_POST[regno]','$_POST[busname]','$_POST[busid]','$_POST[buscolour]','$_POST[busmanufacturer]')";
+	$sql="INSERT INTO bus(Reg_No,Bus_Name,Bus_Id,Bus_Colour,Bus_Manufacturer,Driver_ID,Conductor_ID,Owner_ID) values ('$_POST[regno]','$_POST[busname]','$_POST[busid]','$_POST[buscolour]','$_POST[busmanufacturer]',$_POST[driverid],$_POST[conductorid],$_POST[ownerid])";
 
 	if(mysqli_query($link,$sql))
 	{
@@ -16,3 +16,4 @@
 		echo "ERROR:could not Insert $sql".mysqli_error($link);
 	}
 ?>
+
